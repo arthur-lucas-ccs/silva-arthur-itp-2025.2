93c1209f5a80009dee56865df26d72408080d5d6
Arthur Lucas Costa Ciriaco da Silva <arthur.lucas.127@ufrn.edu.br>
2025-09-23 23:34:58 -0300

    quarto commit

--- listas/semana4-repeticoes-b/problema6.c
+++ listas/semana4-repeticoes-b/problema6.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
+#include <math.h>
+
 int main()
 {
+    int a, b, c;
+
+    for (a = 0; a < 1000; a++)
+    {
+        for (b = 0; b <= a; b++)
+        {
+            c = sqrt((a * a) + (b * b));
+
+            if ((a * a + b * b == c * c) && c < 1000 && a != 0 && b != 0)
+            {
+                printf("a: %d\n", a);
+                printf("b: %d\n", b);
+                printf("c: %d\n", c);
+                printf("----\n");
+            }
+        }
+    }
+    printf("\n");
 }
