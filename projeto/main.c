399f4f4301292e8cc44bf22b210b70714d435e90
Arthur Lucas Costa Ciriaco da Silva <arthur.lucas.127@ufrn.edu.br>
2025-09-29 01:36:42 -0300

    Atualização Projeto

--- projeto/main.c
+++ projeto/main.c
@@ -2,12 +2,19 @@
 #include <stdbool.h>
 #include <math.h>
 
-void set_posicao(bool binario[], int posicao)
+typedef enum
 {
-    binario[posicao] = 1;
+    VALOR_NULO = -1,
+    JOGADOR_O = 0,
+    JOGADOR_X = 1
+} valores;
+
+void set_posicao(valores binario[], int posicao, valores jogador)
+{
+    binario[posicao] = jogador;
 }
 
-void get_tabuleiro(bool binario[])
+void get_tabuleiro(valores binario[])
 {
     for (int i = 0; i < 9; i++)
     {
@@ -15,7 +22,11 @@ void get_tabuleiro(bool binario[])
         {
             printf("\n");
         }
-        if (binario[i])
+        if (binario[i] == JOGADOR_O)
+        {
+            printf("O ");
+        }
+        else if (binario[i] == JOGADOR_X)
         {
             printf("X ");
         }
@@ -26,34 +37,166 @@ void get_tabuleiro(bool binario[])
     }
 }
 
-int binarioParaDecimal(bool binario[])
+int binarioParaDecimal(valores binario[], valores jogada)
 {
     int aux = 0;
     for (int i = 0; i < 9; i++)
     {
-        aux += binario[i] * pow(2, i);
+        if (binario[i] == jogada)
+        {
+            aux += pow(2, i);
+        }
     }
     return aux;
 }
 
-bool verificaGanho(bool binario[])
+bool verificaGanho(valores binario[], valores jogada)
 {
-    int decimal = binarioParaDecimal(binario);
-    if (decimal & 7 == 7 || decimal & 56 == 56 || decimal & 448 == 448 || decimal & 73 == 73 || decimal & 146 == 146 || decimal & 296 == 296 || decimal & 84 == 84 || decimal & 273 == 273)
+    int decimal = binarioParaDecimal(binario, jogada);
+    if ((decimal & 7) == 7 || (decimal & 56) == 56 || (decimal & 448) == 448 || (decimal & 73) == 73 || (decimal & 146) == 146 || (decimal & 296) == 296 || (decimal & 84) == 84 || (decimal & 273) == 273)
     {
         return 1;
     }
     else
         return 0;
 }
+
+bool verificaVazios(valores binario[])
+{
+    for (int i = 0; i < 9; i++)
+    {
+        if (binario[i] == VALOR_NULO)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void copiaVetor(valores vetorC[], valores vetorV[])
+{
+    for (int i = 0; i < 9; i++)
+    {
+        vetorV[i] = vetorC[i];
+    }
+}
+
+void possiveisJogadas(valores binario[], int posicoes[], int *count)
+{
+    *count = 0;
+    for (int i = 0; i < 9; i++)
+    {
+        if (binario[i] == VALOR_NULO)
+        {
+            posicoes[*count] = i;
+            (*count)++;
+        }
+    }
+}
+
+int minimax(valores binario[], valores jogador, valores eu)
+{
+    bool winEu = verificaGanho(binario, eu);
+    bool winJogador = verificaGanho(binario, jogador);
+    if (winEu)
+    {
+        return 1;
+    }
+    else if (winJogador)
+    {
+        return -1;
+    }
+    else if (!verificaVazios(binario))
+    {
+        return 0;
+    }
+
+    int posicoes[9];
+    int count;
+    possiveisJogadas(binario, posicoes, &count);
+
+    if (jogador == eu)
+    { // max
+        int melhor = -1000000;
+        for (int i = 0; i < count; i++)
+        {
+            valores novoVetor[9];
+            copiaVetor(binario, novoVetor);
+            set_posicao(novoVetor, posicoes[i], eu);
+            valores oponente = (eu == JOGADOR_X) ? JOGADOR_O : JOGADOR_X;
+            int valor = minimax(novoVetor, oponente, eu);
+            if (valor > melhor)
+            {
+                melhor = valor;
+            }
+        }
+        return melhor;
+    }
+    else
+    { // min
+        int melhor = +1000000;
+        for (int i = 0; i < count; i++)
+        {
+            valores novoVetor[9];
+            copiaVetor(binario, novoVetor);
+            set_posicao(novoVetor, posicoes[i], jogador);
+            valores oponente = (jogador == JOGADOR_X) ? JOGADOR_O : JOGADOR_X;
+            int valor = minimax(novoVetor, oponente, eu);
+            if (valor < melhor)
+            {
+                melhor = valor;
+            }
+        }
+        return melhor;
+    }
+}
+
+int melhorPosicao(valores binario[], valores eu)
+{
+    int posicoes[9];
+    int count;
+    possiveisJogadas(binario, posicoes, &count);
+
+    if (count == 0)
+        return -1;
+
+    int melhorAcao = posicoes[0];
+    int melhor = -1000000;
+
+    for (int i = 0; i < count; i++)
+    {
+        valores novoVetor[9];
+        copiaVetor(binario, novoVetor);
+        set_posicao(novoVetor, posicoes[i], eu);
+        valores oponente = (eu == JOGADOR_X) ? JOGADOR_O : JOGADOR_X;
+        int valor = minimax(novoVetor, oponente, eu);
+        if (valor > melhor)
+        {
+            melhor = valor;
+            melhorAcao = posicoes[i];
+        }
+    }
+    return melhorAcao;
+}
+
 int main()
 {
-    bool tabuleiro[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-    set_posicao(tabuleiro, 0);
-    set_posicao(tabuleiro, 4);
-    set_posicao(tabuleiro, 8);
-    set_posicao(tabuleiro, 6);
+    valores tabuleiro[9] = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
+
+    // Configuração do tabuleiro
+    set_posicao(tabuleiro, 2, JOGADOR_O);
+    set_posicao(tabuleiro, 3, JOGADOR_X);
+    set_posicao(tabuleiro, 8, JOGADOR_O);
+    set_posicao(tabuleiro, 7, JOGADOR_X);
+    set_posicao(tabuleiro, 0, JOGADOR_X);
+
+    printf("Tabuleiro atual:\n");
     get_tabuleiro(tabuleiro);
-    printf("\n%d\n", binarioParaDecimal(tabuleiro));
-    printf("%d", verificaGanho(tabuleiro));
+    printf("\n\n");
+
+    // Testa melhores jogadas
+    printf("Melhor jogada para X: %d\n", melhorPosicao(tabuleiro, JOGADOR_X));
+    printf("Melhor jogada para O: %d\n", melhorPosicao(tabuleiro, JOGADOR_O));
+
+    return 0;
 }
